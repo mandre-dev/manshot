b7eaa620f32c35bd0b3a001cda1d2d72cc19afc7
mandre <marcosandredev25@gmail.com>
2026-04-18 12:52:20 -0300

    Improve startup script with concurrency control and startup delay

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-celery -A api.tasks.celery_app worker --loglevel=info &
+celery -A api.tasks.celery_app worker --loglevel=info --concurrency=1 &
+sleep 3
 uvicorn api.main:app --host 0.0.0.0 --port $PORT
